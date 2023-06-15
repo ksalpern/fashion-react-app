fea2e68278d42fe47c73d9851a8fd35f55fef404
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-06-15 14:27:08 +0300

    update responsive banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,7 +7,6 @@ const Banner = () => {
     <section className="banner">
     <video className="banner__video-background" autoPlay loop muted>
       <source src="/assets/3.mp4" type="video/mp4" />
-      Your browser does not support the video tag.
     </video>
     <div className="banner__content">
      <h1>Express Yourself through Fashion</h1>
